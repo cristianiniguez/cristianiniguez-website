5b2de03e07f35cfc779876745b53f68ab87aaa7e
cristianiniguez <felizpc1@gmail.com>
2026-03-22 22:54:01 -0400

    using stylistic and eslint recommended configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,27 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import stylistic from '@stylistic/eslint-plugin'
 
 const eslintConfig = defineConfig([
+  js.configs.recommended,
+  stylistic.configs.recommended,
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts'
   ]),
-]);
+  {
+    rules: {
+      '@stylistic/comma-dangle': ['error', 'never']
+    }
+  }
+])
 
-export default eslintConfig;
+export default eslintConfig
